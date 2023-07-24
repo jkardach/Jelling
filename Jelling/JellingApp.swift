7c8aa5277322f2761ec1abc79f67568b8d0265ef
Jim Kardach <jim@kardach.clom>
2023-07-24 13:33:22 -0700

    Initial Commit

---
+++ Jelling/JellingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JellingApp.swift
+//  Jelling
+//
+//  Created by jim kardach on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct JellingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
